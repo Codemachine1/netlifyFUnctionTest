ae2996a9739c54ed4e53c58776c1af2b94e63272
Your Name <you@example.com>
2020-11-11 14:53:51 -0500

    update

---
+++ src/quote.jsx
@@ -0,0 +1,22 @@
+
+
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+export default function Quote(){
+    var [quote,setQuote]=useState("")
+    useEffect(()=>{
+        async function getQuote(){
+            const response = await fetch("/.netlify/functions/getPositiveQuotes");
+            const result = await response.json();
+            setQuote(result.quote);
+        }
+        getQuote()
+    
+    })
+    return(
+    <p>{quote}</p>
+    )
+}
